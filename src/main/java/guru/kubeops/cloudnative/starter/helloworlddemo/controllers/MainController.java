e424e8bafc7b9f089be7115f824499a6a0e67383
dmakeroam <cpfapp@gmail.com>
2020-10-09 18:31:56 +0700

    Updated

---
+++ src/main/java/guru/kubeops/cloudnative/starter/helloworlddemo/controllers/MainController.java
@@ -0,0 +1,20 @@
+package guru.kubeops.cloudnative.starter.helloworlddemo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/helloworld-demo/v1")
+@RefreshScope
+public class MainController {
+
+    @Value("${helloworld.message}")
+    private String message;
+
+    @GetMapping("/print")
+    public String print(){
+        return message;
+    }
+
+}
