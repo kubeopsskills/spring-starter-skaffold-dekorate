406dd3bf5b4d025bdee222bd248b90792c82e6a5
dmakeroam <cpfapp@gmail.com>
2020-10-09 23:31:22 +0700

    Fixed image pull backoff

--- src/main/java/guru/kubeops/cloudnative/starter/helloworlddemo/controllers/MainController.java
+++ src/main/java/guru/kubeops/cloudnative/starter/helloworlddemo/controllers/MainController.java
@@ -3,9 +3,11 @@ package guru.kubeops.cloudnative.starter.helloworlddemo.controllers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/helloworld-demo/v1")
+@RestController
+@RequestMapping("/helloworld-demo/v1")
 @RefreshScope
 public class MainController {
 
