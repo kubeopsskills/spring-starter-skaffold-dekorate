8f13fa3cbd5d67346b43a62dc54c36482bf9f17d
dmakeroam <cpfapp@gmail.com>
2020-10-09 14:39:37 +0700

    Initial here

---
+++ src/main/java/guru/kubeops/cloudnative/starter/helloworlddemo/HelloworldDemoApplication.java
@@ -0,0 +1,13 @@
+package guru.kubeops.cloudnative.starter.helloworlddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/kubeops/cloudnative/starter/helloworlddemo/HelloworldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.kubeops.cloudnative.starter.helloworlddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
